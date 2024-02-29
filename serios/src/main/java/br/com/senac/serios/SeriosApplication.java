be47eeb4770f2ca00521204309127ff4cba07311
Lucas Boni <84343948+LucasBoni29@users.noreply.github.com>
2024-02-28 21:34:49 -0300

    Adicionado projeto.
    Adicionado projeto spring criado no Spring Initializr.

---
+++ serios/src/main/java/br/com/senac/serios/SeriosApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.serios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriosApplication.class, args);
+	}
+
+}
