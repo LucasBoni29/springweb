d3addc7e4c2ad54db70cbe0bc4fd4564db720010
Lucas Boni <santosboni2002@gmail.com>
2024-03-05 00:49:37 -0300

    SP-1004 - Adicionado outras coisas na tela.

--- serios/src/main/java/br/com/senac/serios/controller/Login.java
+++ serios/src/main/java/br/com/senac/serios/controller/Login.java
@@ -1,6 +1,6 @@
 package br.com.senac.serios.controller;
 
-import br.com.senac.serios.data.domain.entity.UsuarioEntity;
+import br.com.senac.serios.data.domain.entity.LoginEntity;
 import br.com.senac.serios.data.domain.repository.LoginRepository;
 import br.com.senac.serios.dto.UsuarioDTO;
 import br.com.senac.serios.service.LoginService;
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -29,21 +28,22 @@ public class Login {
 
     @GetMapping("/validando")
     public String exibirMensagem(Model model){
-        model.addAttribute("usuario", new UsuarioEntity());
+        model.addAttribute("login", new LoginEntity());
         return "/home";
     }
 
     @GetMapping("/home")
     public String home(Model model){
-        model.addAttribute("usuario", new UsuarioEntity());
+        model.addAttribute("login", new LoginEntity());
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@Valid UsuarioEntity usuario, BindingResult result, RedirectAttributes attributes, UsuarioDTO dto){
+    public String login(@Valid LoginEntity loginEntity, BindingResult result, RedirectAttributes attributes, UsuarioDTO dto){
 
         if(result.hasErrors()){
-            return "/home";
+            attributes.addFlashAttribute("mensagem", "E-mail ou senha incorretos!");
+            return "redirect:/home";
         }else if(service.validaLoginUsuario(dto.getEmail(), dto.getSenha())){
             return "/list";
         }
