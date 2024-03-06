44011a55fec8f53c962f6d945f240d099d5d7e58
Lucas Boni <santosboni2002@gmail.com>
2024-03-06 03:09:51 -0300

    SP-1006 - Validação de login do usuário criada e criptografia de senhas também.

--- serios/src/main/java/br/com/senac/serios/controller/Login.java
+++ serios/src/main/java/br/com/senac/serios/controller/Login.java
@@ -1,8 +1,6 @@
 package br.com.senac.serios.controller;
 
-import br.com.senac.serios.data.domain.entity.LoginEntity;
-import br.com.senac.serios.data.domain.repository.LoginRepository;
-import br.com.senac.serios.dto.UsuarioDTO;
+import br.com.senac.serios.data.domain.entity.UsuarioEntity;
 import br.com.senac.serios.service.LoginService;
 import br.com.senac.serios.service.impl.LoginServiceImpl;
 import jakarta.validation.Valid;
@@ -14,41 +12,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
 @Controller
 public class Login {
 
-    private final LoginRepository loginRepository;
     private final LoginService service;
 
     @Autowired
-    public Login(LoginRepository loginRepository, LoginServiceImpl loginService) {
-        this.loginRepository = loginRepository;
+    public Login(LoginServiceImpl loginService) {
         this.service = loginService;
-    }
-
-    @GetMapping("/validando")
-    public String exibirMensagem(Model model){
-        model.addAttribute("login", new LoginEntity());
-        return "/home";
     }
 
     @GetMapping("/home")
     public String home(Model model){
-        model.addAttribute("login", new LoginEntity());
+        model.addAttribute("login", new UsuarioEntity());
         return "login";
     }
 
-    @PostMapping("/login")
-    public String login(@Valid LoginEntity loginEntity, BindingResult result, RedirectAttributes attributes, UsuarioDTO dto){
+    @GetMapping("/pagina-principal")
+    public String paginaPrincipal(){
+        return "pagina-principal";
+    }
 
+    @PostMapping("/login")
+    public String login(@Valid UsuarioEntity usuarioEntity, BindingResult result, RedirectAttributes attributes, Model model) throws NoSuchAlgorithmException {
         if(result.hasErrors()){
-            attributes.addFlashAttribute("mensagem", "E-mail ou senha incorretos!");
+            List<String> listaErros = service.capturarMensagensErros(result);
+            attributes.addFlashAttribute("mensagem", listaErros);
             return "redirect:/home";
-        }else if(service.validaLoginUsuario(dto.getEmail(), dto.getSenha())){
-            return "/list";
+        }else{
+            return service.executarLogin(usuarioEntity.getSenha(), usuarioEntity, attributes, model);
         }
-        attributes.addFlashAttribute("mensagem", "E-mail ou senha inválidos");
-        return "redirect:/validando";
+
     }
 
 }
